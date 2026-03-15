c2511da6c2e6f852d8a31cc8dc17e3a04e92eeb7
hiepphan1411 <22719711.hiep@student.iuh.edu.vn>
2026-03-15 15:06:21 +0700

    fix: main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,14 +16,14 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-    }),
-  );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     transform: true,
+  //     whitelist: true,
+  //   }),
+  // );
 
-  app.enableCors();
+  // app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }
