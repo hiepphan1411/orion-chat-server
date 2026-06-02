8dd4de4a728f20d8759180ba25eb3c9da39b2649
Phan Phuoc Hiep <22719711.hiep@student.iuh.edu.vn>
2026-06-03 00:18:52 +0700

    chore: update hardcode connection

--- src/database/data-sourde.ts
+++ src/database/data-sourde.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '123456789',
-  database: 'orion_chat',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT || 5432),
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || '123456789',
+  database: process.env.DB_NAME || 'orion_chat',
   entities: ['dist/**/*.entity.js'],
-  synchronize: true,
+  synchronize: process.env.TYPEORM_SYNC === 'true',
 });
